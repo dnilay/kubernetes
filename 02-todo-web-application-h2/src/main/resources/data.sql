56c8d77ade56a8f2cfc87fe727adeb410e9766d4
dnilay <mailnilaydas@gmail.com>
2020-12-03 16:11:49 +0530

    final commit

---
+++ 02-todo-web-application-h2/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into TODO
+values(10001, 'Learn Spring Boot', false, sysdate(), 'demo1');
+insert into TODO
+values(10002, 'Learn Angular JS', false, sysdate(), 'demo2');
+insert into TODO
+values(10003, 'Learn to Dance', false, sysdate(), 'demo3');
